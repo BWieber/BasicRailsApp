a77f5c1cfb70d3a0aa14f19f61b875eb17c6ae03
Brandon Wieber <brandon.m.wieber@gmail.com>
2015-11-07 16:01:13 -0500

    Assignment 34 Completed.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,11 +11,11 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
-    @topic = Topic.find(params[:topic_id])
-    @post.topic = @topic
+    @post =        Post.new
+    @post.title =  params[:post][:title]
+    @post.body =   params[:post][:body]
+    @topic =       Topic.find(params[:topic_id])
+    @post.topic =  @topic
 
       if @post.save
             flash[:notice] = "Post was saved."
---
+++ db/migrate/20151107170820_add_topic_to_sponsored_posts.rb
@@ -0,0 +1,7 @@
+class AddTopicToSponsoredPosts < ActiveRecord::Migration
+  def change
+
+    add_column :sponsored_posts, :topic_id, :integer
+    add_index :sponsored_posts, :topic_id
+  end
+end
