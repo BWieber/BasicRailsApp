79438cf4af2a43c3d02fe804d6edd2038b5a0c3c
Brandon Wieber <brandon.m.wieber@gmail.com>
2015-11-09 20:03:06 -0500

    Checkpoint 39 (Posts and Users) Completed.

--- lib/random_data.rb
+++ lib/random_data.rb
@@ -1,5 +1,16 @@
 module RandomData
 
+    def random_name
+      first_name = random_word.capitalize
+      last_name  = random_word.capitalize
+      "#{first_name} #{last_name}"
+    end
+
+    def random_email
+      "#{random_word}@#{random_word}.#{random_word}"
+    end
+
+
     def random_paragraph
       sentences = []
       rand(4..6).times do
@@ -15,9 +26,9 @@ module RandomData
           strings << random_word
         end
 
-        sentence = strings.join(" ")
-        sentence.capitalize << "."
-      end
+      sentence = strings.join(" ")
+      sentence.capitalize << "."
+    end
 
       def random_word
           letters = ('a'..'z').to_a
