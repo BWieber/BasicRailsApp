d309d457977492c68c42385358479273ad523a7c
Brandon Wieber <brandon.m.wieber@gmail.com>
2015-11-15 16:23:00 -0500

    Assignment 44 (Favorites) Completed.

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -12,4 +12,14 @@ class FavoriteMailer < ActionMailer::Base
 
     mail(to: user.email, subject: "New comment on #{post.title}")
   end
+
+  def new_post(post)
+    headers["Message-ID"] = "<posts/#{post.id}@http://localhost:3000/>"
+    headers["In-Reply-To"] = "<post/#{post.id}@http://localhost:3000/>"
+    headers["References"] = "<post/#{post.id}@http://localhost:3000/>"
+
+    @post = post
+
+    mail(to: post.user.email, subject: "New favorite on #{post.title}.")
+  end
 end
