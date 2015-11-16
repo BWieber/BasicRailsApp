57e4d3852e6e34a9a78ccc446752e70e505d8ca5
Brandon Wieber <brandon.m.wieber@gmail.com>
2015-11-16 18:25:45 -0500

    Updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151115175727) do
+ActiveRecord::Schema.define(version: 20151116051141) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -88,6 +88,17 @@ ActiveRecord::Schema.define(version: 20151115175727) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "severities", force: :cascade do |t|
+    t.integer  "rating_id"
+    t.integer  "severity_id"
+    t.string   "severity_type"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "severities", ["rating_id"], name: "index_severities_on_rating_id"
+  add_index "severities", ["severity_type", "severity_id"], name: "index_severities_on_severity_type_and_severity_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
