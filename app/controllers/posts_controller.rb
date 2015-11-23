92c35c06f456260f69a93116aa4b7336424d3790
Xander Miller <xander.miller@gmail.com>
2015-11-23 19:31:26 +0000

    adds rating selector to Post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -62,7 +62,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :severity)
   end
 
   def authorize_user
