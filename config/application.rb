1146ea2762dd1c4bd1a34cc01171b5411cef375f
Brandon Wieber <brandon.m.wieber@gmail.com>
2015-12-31 12:49:28 -0500

    Adjusted application.rb

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require(:default, Rails.env)
 
 module Bloccit
   class Application < Rails::Application
@@ -28,8 +28,5 @@ module Bloccit
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
    end
 end
